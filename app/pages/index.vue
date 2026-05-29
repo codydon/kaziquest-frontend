860c2f8a1c9095f4710b074f08cdc3685168f8b8
Don <donaldonserio20@gmail.com>
2026-05-29 21:50:30 +0300

    add check for server environment in hydration watcher

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,6 +6,7 @@ const { isAuthenticated, isHydrating } = useAuthSession()
 watch(
 	() => isHydrating.value,
 	async (hydrating) => {
+		if (import.meta.server) return
 		if (hydrating) {
 			return
 		}
